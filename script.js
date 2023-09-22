8b5cea10f96fac43f5b31f5d28927cbd1e9c26f4
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-09-22 22:23:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ let elements = []; // Array to hold generated DOM elements
 // Function to generate DOM elements
 const generateElements = () => {
   // complete the function
+	for(let i = 0; i < 1000; i++) {
+		const el = document.createElement("div");
+		el.textContent = `Element ${i}`;
+
+		document.body.appendChild(el);
+	}
   updateMemoryUsage();
 };
 
 // Function to remove DOM elements
 const removeElements = () => {
+	document.querySelectorAll("div").forEach(e => document.body.removeChild(e))
   // complete the function
   updateMemoryUsage();
 };
@@ -18,6 +25,11 @@ const removeElements = () => {
 // Function to update memory usage display
 const updateMemoryUsage = () => {
   // Complete this function
+	if (window.performance && window.performance.memory) {
+	  const memoryInfo = window.performance.memory; 
+
+		document.getElementById("memory").textContent = `Memory usage: ${memoryInfo.usedJSHeapSize / (1024 * 1024) + " MB"} / ${memoryInfo.totalJSHeapSize / (1024 * 1024) + " MB"}`
+	}
 };
 
 // Add event listeners to buttons
